e785b8442e0dd81f432f0abb11523cdfffae71de
Jared Seefried <jared.seefried@yahoo.com>
2020-10-23 20:16:53 -0600

    Updated app.get

--- index.js
+++ index.js
@@ -7,15 +7,20 @@ const db = require("./db/db.json");
 const uuid = require("uuid");
 const PORT = process.env.PORT || 4000;
 
-//Get index.html and notes.html 
+// Express middleware Statements
+//Get files in the public folder 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Routes
-
+app.get("/notes", function(req, res) {
+    // var dbnote = req.params.db
+    // console.log(dbnote);
+    res.end();
+});
 
 //listeners 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
-});
+    console.log("Listening to port:" + PORT);
+}); 
