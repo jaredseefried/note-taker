7b46886bfef135bc937be92ffc92365af8434c3e
Jared Seefried <jared.seefried@yahoo.com>
2020-10-23 20:37:34 -0600

    Fixed button href on index.html

--- index.js
+++ index.js
@@ -8,17 +8,19 @@ const uuid = require("uuid");
 const PORT = process.env.PORT || 4000;
 
 // Express middleware Statements
-//Get files in the public folder 
+// Get files in the public folder 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Routes
-app.get("/api/notes", function(req, res) {
-    // var dbnote = req.params.db
-    // console.log(dbnote);
-    res.end();
-});
+
+// app.get("/api/notes", function(req, res) {
+//     // var dbnote = req.params.db
+//     // console.log(dbnote);
+//     res.end();
+// });
+
 
 //listeners 
 app.listen(PORT, () => {
