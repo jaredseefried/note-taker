5e2a6ae5747c57fbdf35a71d76a4c765b77d73d2
Jared Seefried <jared.seefried@yahoo.com>
2020-10-25 17:11:26 -0600

    Reduced the amount of notes and cleaned up code

--- server.js
+++ server.js
@@ -16,15 +16,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Routes
-// GET the notes in the JSON file
+
+// GET the notes from the db.json file. 
 app.get("/api/notes", function (req, res) {
 
-    // Using the file system statement, Read the JSON File
+    // Using the file system, read the dc.json file, parse the data and send it the browser
     fs.readFile(path.join(__dirname, "", "./db/db.json"), (err, data) => {
         if (err) throw err;
-        // Parse the data into an object
         const notes = JSON.parse(data)
-        // Send the JSON with the notes
         res.send(notes)
         console.log(notes);
     });
@@ -32,38 +31,25 @@ app.get("/api/notes", function (req, res) {
 
 })
 
-// POST the new note into the 'db.json' file
+// POST - Write the new note to the db.json file. 
 app.post("/api/notes", function (req, res) {
-
-    // Read the current 'db.json' file
     fs.readFile(path.join(__dirname, "./db/db.json"), function (err, data) {
         if (err) throw err;
-
-        // Define the variable that will parse the new note into an object
-        const newNote = JSON.parse(data)
-
-        // Push the new note into the Key-Value pairs of data on the JSON file
-        newNote.push(req.body)
-
-        // Define the variable that will Stringify the Note which called from the JSON File
-        const finalNote = JSON.stringify(newNote);
-
-        // Write the object to the JSON file
-        fs.writeFile(path.join(__dirname, "./db/db.json"), finalNote, function (err, data) {
-            if (err) {
-                throw err;
-            }
-
+        const noteObj = JSON.parse(data);
+        noteObj.push(req.body);
+        
+        const noteStr = JSON.stringify(noteObj);
+        fs.writeFile(path.join(__dirname, "./db/db.json"), noteStr, function (err) {
+            if (err) throw err;
         })
-        // Send the JSON response which contains all key-value pairs
         res.json(req.body)
     })
 })
 
-
+// Delete the specified Note
 app.delete("/api/notes", function (req, res) {
 
-    res.end()
+
 })
 
 // Listener
