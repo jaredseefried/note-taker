91784e39579fa288ceef6fdfba0974eecc3484a7
Jared Seefried <jared.seefried@yahoo.com>
2020-10-24 19:19:31 -0600

    committing current changes

--- server.js
+++ server.js
@@ -4,25 +4,38 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const db = require("./db/db.json");
-const uuid = require("uuid");
+// const uuid = require("uuid");
 const PORT = process.env.PORT || 4000;
 
-// Express middleware Statements
 // Get files in the public folder 
 app.use(express.static('public'));
+
+// Set up express app for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Routes
+app.get("/api/notes", function(req, res) {
+    
+    fs.readFile(path.join)(__dirname, "", "./db/db.json"), function(err, data){
+        if (err){
+            throw err;
+        }
+        const notes = JSON.parse(data);
+        res.json(notes);
+    }
 
-// app.get("/api/notes", function(req, res) {
-//     // var dbnote = req.params.db
-//     // console.log(dbnote);
-//     res.end();
-// });
+    res.end();
+    
+});
+
+app.post("/api/notes.html", function (req, res){
+
+})
 
 
 //listeners 
 app.listen(PORT, () => {
     console.log("Listening to port:" + PORT);
+    console.log(db);
 }); 
