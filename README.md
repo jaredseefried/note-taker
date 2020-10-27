a14d79da466fa3a870577ce56bf6ba313b4c9ad8
Jared Seefried <jared.seefried@yahoo.com>
2020-10-27 09:47:52 -0600

    cleaned up code

---
+++ README.md
@@ -0,0 +1,46 @@
+# Note Taker
+
+![Name]([./img or gif])
+
+## Table of Contents
+
+  * **[Description](#Description)**  
+  * **[Installation Requirements](#Installation-Requirements)**  
+  * **[Usage](#Usage)**  
+  * **[License](#License)**    
+  * **[Contributors](#Contributors)**  
+  * **[Tests](#Tests)**  
+  * **[Questions](#Questions)** 
+
+# Description
+
+Create an application that can be used to write, save, and delete notes. This application will use an express backend and save and retrieve note data from a JSON file. AS A user, I want to be able to write and save notes. I WANT to be able to delete notes I've written before. SO THAT I can organize my thoughts and keep track of tasks I need to complete. For users that need to keep track of a lot of information, it's easy to forget or be unable to recall something important. Being able to take persistent notes allows users to have written information available when needed.Application should allow users to create and save notes.
+
+# Installation Requirements
+
+Express, UUID, FS, Path
+
+# Usage
+
+NA
+
+# License 
+
+none
+
+# Contributors
+
+NA
+
+# Tests
+
+NA
+
+# Have Questions?
+
+## [Github Profile: github.com/jaredseefried](https://github.com/jaredseefried "Title")
+
+Please email me at jared.seefried@yahoo.com if you have additional questions. 
+
+### Image of Note Taker
+![alt text](./)
--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
-const app = express();
 const db = require("./db/db.json");
 const uuid = require("uuid")
 
+const app = express();
 const PORT = process.env.PORT || 4001;
 
 // Get files in the public folder 
@@ -16,15 +16,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Routes
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+})
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
 
 // GET the notes from the db.json file. 
 app.get("/api/notes", function (req, res) {
 
     // Using the file system, read the db.json file, parse the data in an object and send it back in a response.
-    fs.readFile(path.join(__dirname, "", "./db/db.json"), (err, data) => {
+    fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
         if (err) throw err;
-        const notes = JSON.parse(data) 
-        res.send(notes)
+        const notes = JSON.parse(data)
+        res.json(notes)
         console.log(notes);
     });
 })
@@ -33,19 +40,19 @@ app.get("/api/notes", function (req, res) {
 app.post("/api/notes", function (req, res) {
 
     // Using the file system, parse the data into an object, get the body of the array, give it an ID and push the note into the JSON file as an object.
-    fs.readFile(path.join(__dirname, "./db/db.json"), function (err, data) {
+    fs.readFile(path.join(__dirname, "/db/db.json"), function (err, data) {
         if (err) throw err;
         const noteObj = JSON.parse(data);
-        const notes = req.body;
-        notes.id = uuid.v1();
-        noteObj.push(notes);
+        const note = req.body;
+        note.id = uuid.v1();
+        noteObj.push(note);
 
         // Turn the note object into a string and write it to the JSON file. 
         const noteStr = JSON.stringify(noteObj);
-        fs.writeFile(path.join(__dirname, "./db/db.json"), noteStr, function (err) {
+        fs.writeFile(path.join(__dirname, "/db/db.json"), noteStr, function (err) {
             if (err) throw err;
+            res.json(req.body)
         })
-        res.json(req.body)
     })
 })
 
@@ -54,15 +61,22 @@ app.delete("/api/notes/:id", function (req, res) {
 
     // Get the note ID, read the JSON file, parse the string into an object, filter through the data and if the ID of what is being deleted does not match > write those to the JSON file and remove the one that did match. 
     const noteID = req.params.id;
-    fs.readFile(path.join(__dirname, "", "./db/db.json"), function (err, data) {
+    fs.readFile(path.join(__dirname, "/db/db.json"), function (err, data) {
         if (err) throw err;
         const notes = JSON.parse(data);
         const deleteNote = notes.filter((noteObj) => noteObj.id !== noteID)
-        fs.writeFile("./db/db.json", JSON.stringify(deleteNote), function (err, data) {
-            if (err) throw err;
+        fs.writeFile("./db/db.json", JSON.stringify(deleteNote), function (err) {
+            if (err) {
+                console.log(err);
+                res.status(500).end();
+            }
+            res.json(deleteNote)
         })
-        res.json(deleteNote)
     })
+})
+
+app.get("*", function (req, res){
+    res.redirect("/")
 })
 
 // Listener
